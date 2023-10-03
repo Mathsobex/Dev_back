0a6e175da3157a6ace9e0bb85efbb2cd27f4e723
MatheusPuc <matheus.silva.1456191@sga.pucminas.br>
2023-10-02 21:23:22 -0400

    Configuração do banco de dados e da tabela veiculo

---
+++ Dev_back/Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dev_back.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar a placa")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar o ano de fabricação")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigátorio informar o ano do modelo")]
+        public int AnoModelo { get; set; }
+    }
+
+}
