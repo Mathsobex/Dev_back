3cf81b468bbe40849b906c6737878c2f2efa5249
MatheusPuc <matheus.silva.1456191@sga.pucminas.br>
2023-10-05 20:49:27 -0400

    l

---
+++ Dev_back/Migrations/20231006004542_M01-AddTableUsuario.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dev_back.Migrations
+{
+    public partial class M01AddTableUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
